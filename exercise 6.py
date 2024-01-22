e4adc4c42050addd348f679a0b5adc557e1b7679
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-22 23:33:29 +0800

    function created, prints numbers that are divisible by 5

--- exercise 6.py
+++ exercise 6.py
@@ -1,2 +1,10 @@
 #Exercise 6: Display numbers divisible by 5 from a list
 #Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+def checking(divisible_by_5_or_not):
+    if divisible_by_5_or_not % 5 == 0:
+        print(divisible_by_5_or_not, "is divisible by 5")
+    
+
+number_list = [10, 20, 33, 46, 55]
+print(checking(number_list[0]))
