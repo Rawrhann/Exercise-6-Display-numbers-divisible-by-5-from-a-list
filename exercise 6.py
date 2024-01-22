0d7a0d70ee053b56e50110c02b0f88605d09410a
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-22 23:35:31 +0800

    for loop creation

--- exercise 6.py
+++ exercise 6.py
@@ -4,7 +4,11 @@
 def checking(divisible_by_5_or_not):
     if divisible_by_5_or_not % 5 == 0:
         print(divisible_by_5_or_not, "is divisible by 5")
+        return
+    else:
+        return
     
 
 number_list = [10, 20, 33, 46, 55]
-print(checking(number_list[0]))
+for i in range(0,5):
+    print(checking(number_list[i]))
